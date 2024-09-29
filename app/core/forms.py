e15b50804c131b8274f4bd701975990a8efdb7ff
Juan Espinoza <carlos_jcez@hotmail.com>
2024-09-29 00:25:55 -0500

    fix clinic save

--- app/core/forms.py
+++ app/core/forms.py
@@ -205,6 +205,21 @@ class ClinicForm(forms.ModelForm):
         self.fields["state"].queryset = State.objects.none()
         self.fields["city"].queryset = City.objects.none()
 
+        if "country" in self.data:
+            try:
+                country_id = int(self.data.get("country"))
+                self.fields["state"].queryset = State.objects.filter(
+                    country_id=country_id
+                )
+            except (ValueError, TypeError):
+                pass
+        if "state" in self.data:
+            try:
+                state_id = int(self.data.get("state"))
+                self.fields["city"].queryset = City.objects.filter(state_id=state_id)
+            except (ValueError, TypeError):
+                pass
+
 
 class ClinicUpdateForm(forms.ModelForm):
     class Meta:
